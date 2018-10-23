0bbd236ea3bff9034583d7692b836bcafcc4ef92
davidp37 <djparra@uci.edu>
2018-10-22 18:19:18 -0700

    Updated T

--- project1.py
+++ project1.py
@@ -116,26 +116,20 @@ def fileTaker_E(names: list, file: str):
 # fileTaker_T: returns List object of text files which contains strings specified by "file"
 
 
-def fileTaker_T(names: [list], file: [str])-> list:
+def fileTaker_T(names: [list], file: str)-> list:
     lis = []
     origin = os.getcwd()
     for p in names:
-        found == False
+        found = False
         if os.path.exists(p) and os.path.dirname(p) != os.getcwd():
             os.chdir(os.path.dirname(p))
         try:
-            infile = open(os.path.basename(p), "r")
-            for line in infile:
-                i = 0
-                for word in line.split():
-                    if word == file.split()[i]:
-                        i += 1
-                        if i == len(file.split()):
-                            # string in "file" found in the text file
-                            lis.append(p)
-                            found = True
-                    else:
-                        i = 0
+            infile = open(os.path.basename(p), "r", encoding="ISO-8859-1")
+
+            if file in infile.read():
+                # string in "file" found in the text file
+                lis.append(p)
+                found = True
         except Exception as exceptObj:
             print("ERROR: ", str(exceptObj), " is not a text file.")
         infile.close()
